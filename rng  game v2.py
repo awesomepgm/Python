d4bb5ba35f7945eee4419d8c1b1e55d8c618b4d6
awesomepgm <awesomepgm@users.noreply.github.com>
2018-12-19 17:41:16 -0800

    Re added since didnt work first time

--- rng  game v2.py
+++ rng  game v2.py
@@ -5,33 +5,69 @@ def player():
 		v= input('>>>')
 		if v in ['quit', 'quit()', 'exit', 'exit()']:
 			quit()
+		if v in ['restart', 'restart()']:
+			f2()
+		if v in ['ya like jazz?', 'ya like jazz']:
+			open_data()
 		p= int(v)
-		return p
+		if p==1:
+			f()
+		else:
+			return p
 	except ValueError:
-		print('*Error: Input is not a number.')
-		player()
+		if v in ['ya like jazz?', 'ya like jazz']:
+			print('yes, parsa like jazz')
+		else:
+			print('*Error: Input is not a number.')
+			player()
 def f():
 	import random
+	print('Enter the amount of rounds you want to play!')
 	try:
-		print('Enter a number from 1 to 10!:')
-		h= input('>>>')
-		if h in ['quit', 'quit()', 'exit', 'exit()']:
+		s= input('>>>')
+		if s in ['quit', 'quit()', 'exit', 'exit()']:
 			quit()
-		cool= int(h)
-		x=random.randint(1,10)
-		print('*The number was:', x)
-		print('*The difference is:',abs(x-cool))
+		if s in ['restart', 'restart()']:
+			f2()
+		if s in ['ya like jazz?', 'ya like jazz']:
+			open_data()
+		r= int(s)
 	except ValueError:
-		print('*Error: Input is not a number.')
-		f()
+		if s in ['ya like jazz?', 'ya like jazz']:
+			print('yes, parsa like jazz')
+		else:
+			print('*Error: Input is not a number.')
+			f2()
+	for adhiuahdih in range(0,r):
+		try:
+			print('Enter a number from 1 to 10!:')
+			h= input('>>>')
+			if h in ['quit', 'quit()', 'exit', 'exit()']:
+				quit()
+			if h in ['restart', 'restart()']:
+				f2()
+			if h in ['ya like jazz?', 'ya like jazz']:
+				open_data()
+			cool= int(h)
+			x=random.randint(1,10)
+			print('*The number was:', x)
+			print('*The difference is:',abs(x-cool))
+		except ValueError:
+			if h in ['ya like jazz?', 'ya like jazz']:
+				print('yes, parsa like jazz')
+			else:
+				print('*Error: Input is not a number.')
+				f()
 	ifs()
 def ifs():
 	print('Do you want to play again?:')
 	z= input('>>>')
-	if z in ['y', 'Y', 'yes', 'Yes', 'YES', 'YEs', 'yEs', 'yeS', 'YeS', 'yES']:
+	if z in ['y', 'Y', 'yes', 'Yes', 'YES', 'YEs', 'yEs', 'yeS', 'YeS', 'yES', 'restart', 'restart()']:
 		f2()
 	if z in ['n', 'N', 'no', 'No', 'NO', 'nO', '0', 'quit', 'quit()']:
 		quit()
+	if z in ['ya like jazz?', 'ya like jazz']:
+		open_data()
 	else:
 		print('*Error: Input not valid. Input should be yes or no.')
 		ifs()
@@ -40,51 +76,70 @@ def f2():
 	l=random.randint(1,10)
 	p=player()
 	p1=[]
-	cools=[]
 	print('Enter the amount of rounds you want to play!')
 	try:
 		s= input('>>>')
 		if s in ['quit', 'quit()', 'exit', 'exit()']:
 			quit()
+		if s in ['restart', 'restart()']:
+			f2()
+		if s in ['ya like jazz?', 'ya like jazz']:
+			open_data()
 		r= int(s)
 	except ValueError:
-		print('*Error: Input is not a number.')
-		f2()
+		if s in ['ya like jazz?', 'ya like jazz']:
+			print('yes, parsa like jazz')
+		else:
+			print('*Error: Input is not a number.')
+			f2()
 	for asiod in range(0,p):
 		p1.append(0)
-	for lk in range(1, (r+1)):
+	for lk in range(0, r):
+		cools=[]
 		cool1=[]
 		for x in range(1, (p+1)):
-			if p==1:
-				f()
 			y=str(x)
 			try:
 				print('Player '+y+': Enter a number from 1 to 10!:')
 				h= input('>>>')
 				if h in ['quit', 'quit()', 'exit', 'exit()']:
 					quit()
+				if h in ['restart', 'restart()']:
+					f2()
+				if h in ['ya like jazz?', 'ya like jazz']:
+					open_data()
 				hi=int(h)
 				cool1.append(hi)
 			except ValueError:
-				print('*Error: Input is not a number.')
-				f2()
+				if h in ['ya like jazz?', 'ya like jazz']:
+					print('yes, parsa like jazz')
+				else:
+					print('*Error: Input is not a number.')
+					f2()
 		print('*The number was:', l)
 		for u in range(0, p):
 			m= str(u+1)
 			j= int(cool1[u])
 			print('*Player '+m+': The difference is:',abs(l-j))
 		for meme in range(0, p):
-			joe= int(cool1[meme])
 			memes= str(meme+1)
 			cools.append(abs(l-cool1[meme]))
 		for anime in range(0, p):
+			joe= int(cool1[anime])
 			animoo=str(anime+1)
-			if (int(min(cools))) == l-int(cool1[anime]):
+			if (int(min(cools))) == abs(l-joe):
 				print('*Player '+animoo+' wins the round!')
 				p1[anime]=p1[anime]+1
 	for uwu in range(0,p): #i ran out of variables
 		owo=str(uwu+1)
 		if (max(p1)) == p1[uwu]:
-			print('*Player '+owo+' wins the game!!')
+			print('**Player '+owo+' wins the game!!**')
 	ifs()
+def open_data():
+	import os.path
+	my_path = os.path.abspath(os.path.dirname(__file__))
+	wierd=(my_path+"..\\data\\data1.mp4")
+	wierd2=str(wierd)
+	os.startfile(wierd2)
+	f2()
 f2()
